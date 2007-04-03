0a0238f05c2cf8d956d4009d62663436932abb9a
Graeme Mathieson <mathie@woss.name>
2007-04-03 15:18:47 +0000

    Implementation for yet another form builder.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/yet_another_form_builder@42 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- init.rb
+++ init.rb
@@ -1,1 +1,8 @@
-# Include hook code here
+require 'rubaidh/form_helper'
+
+ActionView::Base.send                 :include, Rubaidh::FormHelper
+ActionView::Helpers::InstanceTag.send :include, Rubaidh::InstanceTag
+ActionView::Helpers::FormBuilder.send :include, Rubaidh::FormBuilderMethods
+
+# And set as the default form builder.
+ActionView::Base.default_form_builder = Rubaidh::FormBuilder
--- tasks/yet_another_form_builder_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :yet_another_form_builder do
-#   # Task goes here
-# end
